accff175f8ed173340d14e4cd82216bc2ba47b7c
emjohnson3 <emjohnson3@madisoncollege.edu>
2023-05-05 23:23:11 -0500

    Banana test part 3

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const App = () => {
   return (
     <div className="App">      
       <input
-        placeholder="banana"
+        placeholder="bananatest"
         onChange={e => updateInputValues('start', e.target.value)}
       />
       <input
